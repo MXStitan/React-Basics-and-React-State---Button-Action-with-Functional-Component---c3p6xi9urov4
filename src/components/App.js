54ce3362b4ce128912136c55a06c139113d4a825
MXStitan <97943946+MXStitan@users.noreply.github.com>
2022-04-12 21:09:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[para, setpara]=useState("")
+  const textChange=()=>{
+  setpara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button onClick={textChange} id="click">Click</button>
+<pre id="para">{para}</pre>
     </div>
   );
 }
